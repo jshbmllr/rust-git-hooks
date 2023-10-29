48c1d3e3f4895f0e7b88b4138c218b899a8a9696
Josh Miller <miller.joshuab@gmail.com>
2023-10-29 11:27:32 -0500

    Initial commit

---
+++ src/logger.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::io::{stdin, BufRead};
+
+fn main() {
+    log();
+}
+
+// This consumes stdin. Do not call this if you need to use stdin.
+fn log() {
+    let name_arg = env::args().nth(0).unwrap_or(String::from("unknown"));
+    let args: Vec<_> = env::args().skip(1).collect();
+    println!("{} called with {:?}", name_arg, args);
+
+    println!("BEGIN STDIN");
+    let stdin = stdin();
+    for line in stdin.lock().lines() {
+        println!("{:?}", line);
+    }
+    println!("END STDIN");
+}
